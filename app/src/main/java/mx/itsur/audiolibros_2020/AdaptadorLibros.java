ce71373e42803d91a41842ac6e79f71ce1de3124
Rafa <rafa2517@hotmail.com>
2020-10-15 09:36:14 -0500

    Carga del RecyclerView en la actividad principal

---
+++ app/src/main/java/mx/itsur/audiolibros_2020/AdaptadorLibros.java
@@ -0,0 +1,58 @@
+package mx.itsur.audiolibros_2020;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Vector;
+
+public class AdaptadorLibros extends RecyclerView.Adapter<AdaptadorLibros.ViewHolder> {
+
+    private Vector<Libro> vectorLibros;
+    private Context contexto;
+    private LayoutInflater inflador;
+
+    public AdaptadorLibros(Context context, Vector<Libro> vectorLibros){
+        inflador = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.vectorLibros = vectorLibros;
+        this.contexto = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = inflador.inflate(R.layout.elemento_selector, null);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Libro libro = vectorLibros.get(position);
+        holder.portada.setImageResource(libro.recursoImagen);
+        holder.titulo.setText(libro.titulo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return vectorLibros.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView portada;
+        public TextView titulo;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            portada = (ImageView) itemView.findViewById(R.id.portada);
+            portada.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            titulo = (TextView) itemView.findViewById(R.id.titulo);
+        }
+    }
+}
---
+++ app/src/main/java/mx/itsur/audiolibros_2020/MainActivity.java
@@ -0,0 +1,29 @@
+package mx.itsur.audiolibros_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler);
+
+        layoutManager = new GridLayoutManager(this, 2);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        AdaptadorLibros adaptadorLibros = new AdaptadorLibros(this, Libro.ejemploLibros());
+        recyclerView.setAdapter(adaptadorLibros);
+
+
+    }
+}
